01718f3f85825548c5fed30988fcf671f2efd8aa
luisa <luisaheise3277@gmail.com>
2022-09-07 15:12:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo_turing.png'
+import Similaridades from './Similaridades.js'
+import { Typography, Paper, Grid, Divider } from '@mui/material';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='main-container'>
+        <Paper style={{width: "90%", alignSelf: "center"}} elevation={0} variant="outlined">
+        <Grid>
+        <Grid display="flex" justifyContent="center" alignItems="center" xs={8}>
+            <img src={logo} style={{width: "20%"}}></img>
+          </Grid>
+          <Grid display="flex" justifyContent="center" alignItems="center" xs={8}>
+            <h1 className='main-title'> Presidenciáveis </h1>
+          </Grid>
+          <Grid display="flex" justifyContent="center" alignItems="center" xs={8}>
+            <h2 className='subtitle' > Por Turing USP </h2>
+            <Divider light />
+          </Grid>
+          <Grid display="flex" justifyContent="center" alignItems="center" xs={8}>
+            <h4 className='general-text'>
+              Bem-vindos ao site do projeto de Presidenciáveis do Turing USP. Nesse projeto, nós coletamos tweets desde 2012 dos candidatos a 
+              presidência da república em 2022 e utilizando técnicas de estado da arte em processamento de linguagem natural, calculamos a similaridade 
+              dos discursos por tema dos candidatos. Caso tenha curiosidade de como isso foi feito, leia nosso artigo no medium sobre o assunto.
+            </h4>
+          </Grid>
+          <Grid xs={8}>
+            <Divider light></Divider>
+          </Grid>
+          <Grid display="flex" justifyContent="center" alignItems="center" xs={8}>
+            <h4 className='general-text'>
+              Abaixo, escolha um assunto da sua preferência e observe as similaridades dos tweets dos candidatos!
+            </h4>
+          </Grid>
+          <Grid justifyContent="center" alignItems="center" xs={8}>
+            <Similaridades/>
+          </Grid>
+        </Grid>  
+        </Paper>
+      </div>
+      
   );
 }
 
---
+++ src/Similaridades.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+function Similaridades() {
+    const [selectedGraficoSimilaridade, setSelectedGraficoSimilaridade] = useState(0);
+    const [graficosSimilaridadeInfo, setGraficosSimilaridadeInfo] = useState(
+        [
+            ["Aposentadoria", "graficos_similaridade/aposentadoria.html"],
+            ["Auxílio Brasil", "graficos_similaridade/auxilio_brasil.html"],
+            ["CLT", "graficos_similaridade/clt.html"],
+            ["Covid-19", "graficos_similaridade/covid.html"],
+            ["Dólar", "graficos_similaridade/dolar.html"],
+            ["Educação", "graficos_similaridade/educacao.html"],
+            ["Fome", "graficos_similaridade/fome.html"],
+            ["Gasolina", "graficos_similaridade/gasolina.html"],
+            ["Inflação", "graficos_similaridade/inflacao.html"],
+            ["LGBT", "graficos_similaridade/lgbt.html"],
+            ["PEC Kamikaze", "graficos_similaridade/pec_kamikaze.html"],
+            ["Religião", "graficos_similaridade/religiao.html"],
+            ["Vacina", "graficos_similaridade/vacina.html"]
+        ]
+    );
+
+    const handleThemeChange = (event) => {
+        console.log(event.target.value)
+        setSelectedGraficoSimilaridade(event.target.value);
+      };
+  
+    return (
+        <>
+            <Box style={{width: "95%", margin: "auto"}}>
+            <FormControl fullWidth>
+            <InputLabel id="input-label-select-sim-graph">Tema</InputLabel>
+            <Select
+                labelId="select-label-select-sim-graph"
+                id="sim-graph-select"
+                value={graficosSimilaridadeInfo[selectedGraficoSimilaridade][0]}
+                label="Tema"
+                onChange={handleThemeChange}
+            >
+                {
+                    graficosSimilaridadeInfo.map(function(graphInfo, index){
+                        return <MenuItem value={ index }>{graphInfo[0]}</MenuItem>;
+                        }
+                    )
+                }
+            </Select>
+            </FormControl>
+        </Box>
+        <iframe title ={graficosSimilaridadeInfo[selectedGraficoSimilaridade][0]}
+                id="graficoSimID"
+                src= {graficosSimilaridadeInfo[selectedGraficoSimilaridade][1]}
+                frameborder="0"/>   
+      </>
+      
+    );
+  }
+
+export default Similaridades;
